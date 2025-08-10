5c8f7af3bf5077abe4114d655a368018fdb60d69
Mathijs Dickson <md@outlook.com>
2025-08-10 18:18:41 +1000

    2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to compute the floor of a
+floating-point number.
 
+The `floor` function takes a float and returns the greatest 
+integer less than or equal to the input.
+"""
 
 
 import math
