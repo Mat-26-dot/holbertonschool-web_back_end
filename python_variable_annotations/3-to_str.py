4a27870ab4fa69644a6e41e263a8d2baba4e5f88
Mathijs Dickson <md@outlook.com>
2025-08-10 19:50:23 +1000

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#!/usr/bin/env python3
+"""
+Module for converting a float number to its string representation.
+
+Contains:
+- to_str(n: float) -> str: Converts a float to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """calling function with built-in function str()"""
+    result = str(n)
+    """Returned result after the function was called"""
+    return result
