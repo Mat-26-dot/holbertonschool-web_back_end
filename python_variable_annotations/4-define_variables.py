365b95b8a6cf34473295dcc61f4a3b4f2d4838db
Mathijs Dickson <md@outlook.com>
2025-08-10 20:04:32 +1000

    4-define_variables.py

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module defines some example variables with type annotations:
+
+- a: an integer representing a simple numeric value.
+- pi: a float representing the mathematical constant π approximately.
+- i_understand_annotations: a boolean flag indicating understanding of type annotations.
+- school: a string containing the name of the school.
+"""
+
+
+a = 1
+pi = 3.14
+i_understand_annotations = True
+school = "Holberton"
