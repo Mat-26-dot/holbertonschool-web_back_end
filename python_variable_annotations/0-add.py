8fb122226e6d67646750af05649df119f08c0836
Mathijs Dickson <md@outlook.com>
2025-08-10 16:34:32 +1000

    0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
-
 """This module 'Add' defines the function
 and a, b expected to return a float by the '->'"""
+
+
 def add(a: float, b: float) -> float:
     return a + b
