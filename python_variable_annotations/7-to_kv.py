c9c9be3f204a8e4b49f251c305948c3c50993ab7
Mathijs Dickson <md@outlook.com>
2025-08-11 13:06:29 +1000

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
+"""This module to_kv takes a string, int or float as args
+and returns a tuple"""
 
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """the square root of the tuple value was returned"""
     return (k, float(v**2))
