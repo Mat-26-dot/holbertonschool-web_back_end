8dfcfae1dd65525eb7548fadc00d1745eba3ead8
Mathijs Dickson <md@outlook.com>
2025-08-11 21:27:39 +1000

    9-element_length.py

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
-    Return a list of tuples containing each element from the input iterable 
+    Return a list of tuples containing each element from the input iterable
     and its corresponding length.
 
     Args:
-        lst (Iterable[Sequence]): An iterable of sequence-like objects 
+        lst (Iterable[Sequence]): An iterable of sequence-like objects
             (e.g., strings, lists, tuples) where each element supports len().
 
     Returns:
-        List[Tuple[Sequence, int]]: A list of tuples where each tuple contains 
+        List[Tuple[Sequence, int]]: A list of tuples where each tuple contains
         the original element and an integer representing its length."""
 
 from typing import List, Tuple, Iterable, Sequence
