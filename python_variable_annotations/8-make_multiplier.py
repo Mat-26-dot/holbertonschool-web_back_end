7208acb11ce3df197da2086cc96147ad19b94764
Mathijs Dickson <md@outlook.com>
2025-08-11 16:01:11 +1000

    8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,8 @@
     Creates a multiplier function that multiplies its
     input by a fixed multiplier.
 """
+
+
 from typing import Callable
 
 
@@ -12,10 +14,9 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
                 multiplier (float): The number to multiply inputs by."""
 
         return value * multiplier
-    
-    
     return multiplier_function
 """Returns:
         Callable[[float], float]: A function that takes a float
         and returns the product of that float and the multiplier.
     """
+
