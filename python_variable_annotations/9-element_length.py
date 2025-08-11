35f541531b6a9ff2bdc8f313c797f5df3992bc8d
Mathijs Dickson <md@outlook.com>
2025-08-11 21:12:01 +1000

    9-element_length.py

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,6 @@
 
 from typing import List, Tuple
 
-def element_length(lst: List[]) -> List[Tuple[str, int]]:
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     
     return [(i, len(i)) for i in lst]
