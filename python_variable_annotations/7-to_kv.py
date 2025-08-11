23a8af82a187d011909906848c3ac78fb0f3c0a5
Mathijs Dickson <md@outlook.com>
2025-08-11 13:03:05 +1000

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,5 +3,6 @@
 
 from typing import Union, Tuple
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
-    return (k, v)
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    return (k, float(v**2))
