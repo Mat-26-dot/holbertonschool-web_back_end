33d3ebee0632d959b3c2af26835f79b84a4dc209
Mathijs Dickson <md@outlook.com>
2025-08-13 08:30:20 +1000

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""Module for running multiple asynchronous wait_random calls.
+This module contains the async routine wait_n which calls wait_random 
+n times concurrently with the given maximum delay, and gathers 
+the delays in ascending order without explicitly sorting them."""
+
 
 from basic_async_syntax import wait_random
 from typing import List
@@ -6,19 +11,28 @@ import asyncio
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n times with the given max_delay and return
+    the list of all the delays in ascending order without using sort().
     
-    #Step 1
+    The ascending order is achieved by using asyncio.as_completed(),
+    which yields results as soon as each task is completed.""" 
+    
     empty_list = []
 
-    #Step 2
+
     tasks  = [asyncio.create_task(wait_random(max_delay))for i in range(n)]
 
     #Step 3
     results = []
     for task in asyncio.as_completed(tasks):
+        """The ascending order is achieved by using asyncio.as_completed(),
+        which yields results as soon as each task is completed."""
         result = await task
         results.append(result)
-
+    """Returns:
+        List[float]: list of delays in ascending order.
+    """
     return (results)
 
 
