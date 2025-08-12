854d5c811a652a2719d1072387525761206ad620
Mathijs Dickson <md@outlook.com>
2025-08-12 12:35:11 +1000

    0-basic_async_syntax.py

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import asyncio, random
+
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+print(asyncio.run(wait_random()))
