90f419c9efb86651b5c0478da6399fe7f0246898
Mathijs Dickson <md@outlook.com>
2025-08-12 15:39:07 +1000

    1-concurrent_coroutines.py

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from basic_async_syntax import wait_random
+
+import asyncio
+
+async def wait_n(n, max_delay) -> int:
+    
+    #Step 1
+    empty_list = []
+
+    #Step 2
+    tasks  = [asyncio.create_task(wait_random(max_delay))for i in range(n)]
+
+    #Step 3
+    results = []
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        results.append(result)
+
+    return (results)
+
+
+    
+
+
+
