c10f3b7b2c9df52f74ce7b5cec8b9083ed88c6b2
Mathijs Dickson <md@outlook.com>
2025-08-14 12:22:35 +1000

    0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,13 +2,14 @@
 """This module will loop 10 times, each time asynchronously"""
 import random
 import asyncio
+from typing import AsyncGenerator
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float]:
     """coroutine defined asynchronously"""
-    for i in range(10): 
+    for i in range(10):
         """loop count to 10"""
-        result = random.uniform(0, 10) 
+        result = random.uniform(0, 10)
         """float count count to 10"""
         await asyncio.sleep(1)
-        yield result 
+        yield result
         """result form float count yielded"""
