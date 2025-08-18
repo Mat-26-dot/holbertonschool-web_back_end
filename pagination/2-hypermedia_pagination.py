645f6e4b9008c548c34b6e7fa161654f7e7d3cb6
Mathijs Dickson <md@outlook.com>
2025-08-18 13:15:32 +1000

    2-hypermedia_pagination.py

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+
+#!/usr/bin/env python3
+"""A method named get_page that takes two integer arguments
+page with default value 1 and page_size"""
+
+
+import csv
+import math
+from typing import List
+from simple_helper_function import index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of the dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        """Using index_range page, page_size to pull data"""
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        """Returned data fro mstart and end of index_range"""
+        return data[start:end]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of the dataset"""
+        data = self.get_page (page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        return {
+        
+        "page_size": len(data),
+        "page": page,
+        "data": data,
+        "next_page": page + 1 if page < total_pages else None,
+        "prev_page": page - 1 if page > 1 else None,
+        "total_pages": total_pages,
+        }
