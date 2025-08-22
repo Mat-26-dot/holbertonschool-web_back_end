52b0e9ad4a2c9070b985841dd373fd00ce57fec3
Mathijs Dickson <md@outlook.com>
2025-08-22 11:05:58 +1000

    9-insert_school.py

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" MongoDB insert """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection based on kwargs"""
+    if len (kwargs) == 0:
+        return None
+    return mongo_collection.insert(kwargs)
