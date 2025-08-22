81b152291797ff5c4d04e167dee00e907a7b3168
Mathijs Dickson <md@outlook.com>
2025-08-22 13:15:08 +1000

    10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that changes all topics of a
+school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Function matching school name to set their topics field."""
+
+    result = mongo_collection.update_many(
+      """Modifies multiple documents in a collection"""
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    """returns pymongo collection object"""
+    return result
