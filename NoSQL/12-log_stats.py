12d604e2fd7a4b32573f0e2f147f02b0fc383600
Mathijs Dickson <md@outlook.com>
2025-08-22 15:03:21 +1000

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
-
+"""provides some stats about Nginx logs stored in MongoDB"""
 
 from pymongo import MongoClient
 
 
-# Connect to MongoDB running on localhost
+"""Connect to MongoDB running on localhost"""
 client = MongoClient('mongodb://localhost:27017/')
 db = client.logs
 collection = db.nginx
 
-# Count total number of documents in the collection
+"""Count total number of documents in the collection"""
 count = collection.count_documents({})
 
-# List of HTTP methods to count
+"""List of HTTP methods to count"""
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-# Count documents for each HTTP method
+"""Count documents for each HTTP method"""
 method_counts = {method: collection.count_documents
                 ({"method": method}) for method in methods}
 
-# Count documents where method is GET and path is /status
+"""Count documents where method is GET and path is /status"""
 get_status_count = collection.count_documents({"method": "GET",
                                             "path": "/status"})
 
-# Print results in requested format
+"""Print results in requested format"""
 print(f"{count} logs")
 print("Methods:")
 for method in methods:
