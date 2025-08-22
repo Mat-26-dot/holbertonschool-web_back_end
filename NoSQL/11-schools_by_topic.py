71e41f97a6425a4b81d201bfde4faeafbbefb90d
Mathijs Dickson <md@outlook.com>
2025-08-22 13:53:48 +1000

    11-schools_by_topic.py

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns a list of schools that have the given topic.
+
+    Args:
+        mongo_collection: pymongo collection object
+        topic: string representing the topic to search for
+
+    Returns:
+        List of school documents (dict) matching the topic
+    """
+    result = mongo_collection.find({"topics": topic})
+
+    return result
