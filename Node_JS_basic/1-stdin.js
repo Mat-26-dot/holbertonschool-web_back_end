a0ba96d3a3d1b3af82b40f4c4f4c20a1e741b6ad
Mathijs Dickson <md@outlook.com>
2025-09-02 12:24:40 +1000

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,28 +1,14 @@
-#!/usr/bin/env node
-
-// Display welcome message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Set stdin to readable mode
 process.stdin.setEncoding('utf8');
-process.stdin.resume();
 
-// Handle data input
-process.stdin.on('data', (data) => {
-  // Remove newline character from input
-  const name = data.toString().trim();
-  
-  // Display the user's name
-  process.stdout.write(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Handle end of input
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-});
-
-// Handle SIGINT (Ctrl+C)
-process.on('SIGINT', () => {
-  process.stdout.write('\nThis important software is now closing\n');
-  process.exit();
 });
